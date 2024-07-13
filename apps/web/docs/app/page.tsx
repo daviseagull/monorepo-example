a7fc6a2ad40abb09ef94c456f9df0f1da82b2cbf
Davi Seagull <daviseagull@icloud.com>
2024-07-13 19:02:22 -0400

    chore: add shadcn and fix eslint

---
+++ apps/web/docs/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@monorepo/ui/components/ui/button'
+
+export default function Page() {
+  return (
+    <main>
+      <Button variant="default">Click me</Button>
+    </main>
+  )
+}
--- configs/eslint/react.mjs
+++ configs/eslint/react.mjs
@@ -1,10 +1,16 @@
-import { fixupConfigRules } from "@eslint/compat";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-import globals from "globals";
-import { baseConfig } from "./base.mjs";
+import { fixupConfigRules } from '@eslint/compat'
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
+import globals from 'globals'
+import { baseConfig } from './base.mjs'
 
 export const reactConfig = [
   ...baseConfig,
   ...fixupConfigRules(pluginReactConfig),
   { languageOptions: { globals: { ...globals.browser } } },
-];
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
+    },
+  },
+]
