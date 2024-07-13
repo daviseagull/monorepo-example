39aeec3f29b0a5940f5939771d675ce5144ef0e1
Davi Seagull <daviseagull@icloud.com>
2024-07-13 19:12:53 -0400

    fix: react eslint

--- configs/eslint/react.mjs
+++ configs/eslint/react.mjs
@@ -7,6 +7,7 @@ export const reactConfig = [
   ...baseConfig,
   ...fixupConfigRules(pluginReactConfig),
   { languageOptions: { globals: { ...globals.browser } } },
+  { ignores: ['.next'] },
   {
     rules: {
       'react/react-in-jsx-scope': 'off',
